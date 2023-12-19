192f89aa2717b1b3698c47a2908fd8d8932c71d7
Gerwin de Groot <g.dgroot@magnus.nl>
2023-12-19 16:04:28 +0100

    https://github.com/dotnet/maui/issues/19496

---
+++ WindowsShellFlyoutWidth/MauiApp2/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp2 {
+    public partial class AppShell : Shell {
+        public AppShell() {
+            BindingContext = this;
+            InitializeComponent();
+        }
+
+        private int _counter;
+        public int Counter { get => _counter; set { _counter = value; OnPropertyChanged(); } }
+    }
+}
---
+++ WindowsShellFlyoutWidth/MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiApp2 {
+    public partial class MainPage : ContentPage {
+        private int count;
+
+        public MainPage() {
+            InitializeComponent();            
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e) {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            ((AppShell)Shell.Current).Counter = count;
+        }
+    }
+
+}
